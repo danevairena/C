e53c6433f46ecb97dc16d2f4e90cd5a9ffd700f8
Irena Daneva <de.luxe9997@gmail.com>
2022-12-06 01:11:23 +0200

    Update inheritance.c

--- inheritance.c
+++ inheritance.c
@@ -1,4 +1,21 @@
-// Simulate genetic inheritance of blood type
+/* A person’s blood type is determined by two alleles (i.e., different forms of a gene). The three possible alleles are A, B, and O, of which each 
+person has two (possibly the same, possibly different). Each of a child’s parents randomly passes one of their two blood type alleles to their child. 
+The possible blood type combinations, then, are: OO, OA, OB, AO, AA, AB, BO, BA, and BB.
+For example, if one parent has blood type AO and the other parent has blood type BB, then the child’s possible blood types would be AB and OB, 
+depending on which allele is received from each parent. Similarly, if one parent has blood type AO and the other OB, then the child’s possible 
+blood types would be AO, OB, AB, and OO.
+
+Complete the implementation of inheritance.c, such that it creates a family of a specified generation size and assigns blood type alleles to each family member. 
+The oldest generation will have alleles assigned randomly to them.
+- create_family function takes an integer (generations) as input and should allocate (as via malloc) one person for each member of the family of that number 
+of generations, returning a pointer to the person in the youngest generation. For example, create_family(3) should return a pointer to a person with
+two parents, where each parent also has two parents. Each person should have alleles assigned to them. The oldest generation should have alleles 
+randomly chosen (as by calling the random_allele function), and younger generations should inherit one allele (chosen at random) from each parent.
+Each person should have parents assigned to them. The oldest generation should have both parents set to NULL, and younger generations should have parents 
+be an array of two pointers, each pointing to a different parent.
+- free_family function should accept as input a pointer to a person, free memory for that person, and then recursively free memory for all of their ancestors.
+Since this is a recursive function, you should first handle the base case. If the input to the function is NULL, then there’s nothing to free, 
+so your function can return immediately.Otherwise, you should recursively free both of the person’s parents before freeing the child. */
 
 #include <stdbool.h>
 #include <stdio.h>
