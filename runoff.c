4ac4a596d50de84703f1d25927c7baf83db776be
Irena Daneva <de.luxe9997@gmail.com>
2022-12-06 00:43:22 +0200

    Update runoff.c

--- runoff.c
+++ runoff.c
@@ -1,3 +1,34 @@
+/* You already know about plurality elections, which follow a very simple algorithm for determining the winner of an election: 
+every voter gets one vote, and the candidate with the most votes wins.
+But the plurality vote does have some disadvantages. What happens, for instance, in an election with three candidates, and the ballots below are cast?
+A plurality vote would here declare a tie between Alice and Bob, since each has two votes. But is that the right outcome?
+There’s another kind of voting system known as a ranked-choice voting system. In a ranked-choice system, voters can vote for more than one candidate. 
+Instead of just voting for their top choice, they can rank the candidates in order of preference.
+Here, each voter, in addition to specifying their first preference candidate, has also indicated their second and third choices. 
+And now, what was previously a tied election could now have a winner.
+One such ranked choice voting system is the instant runoff system. In an instant runoff election, voters can rank as many candidates as they wish. 
+If any candidate has a majority (more than 50%) of the first preference votes, that candidate is declared the winner of the election.
+If no candidate has more than 50% of the vote, then an “instant runoff” occurrs. The candidate who received the fewest number of votes 
+is eliminated from the election, and anyone who originally chose that candidate as their first preference now has their second preference considered. 
+Why do it this way? Effectively, this simulates what would have happened if the least popular candidate had not been in the election to begin with.
+The process repeats: if no candidate has a majority of the votes, the last place candidate is eliminated, and anyone who voted for them will instead 
+vote for their next preference (who hasn’t themselves already been eliminated). Once a candidate has a majority, that candidate is declared the winner.
+
+Complete the implementation of runoff.c in such a way that it simulates a runoff election. 
+- vote - The function takes arguments voter, rank, and name. If name is a match for the name of a valid candidate, then you should 
+update the global preferences array to indicate that the voter voter has that candidate as their rank preference (where 0 is the first preference, 1 is 
+the second preference, etc.). If the preference is successfully recorded, the function should return true; the function should return false otherwise 
+(if, for instance, name is not the name of one of the candidates). You may assume that no two candidates will have the same name.
+- tabulate - The function should update the number of votes each candidate has at this stage in the runoff.
+Recall that at each stage in the runoff, every voter effectively votes for their top-preferred candidate who has not already been eliminated.
+- print_winner - If any candidate has more than half of the vote, their name should be printed and the function should return true.
+If nobody has won the election yet, the function should return false.
+- find_min - The function should return the minimum vote total for any candidate who is still in the election.
+- is_tie - The function takes an argument min, which will be the minimum number of votes that anyone in the election currently has.
+The function should return true if every candidate remaining in the election has the same number of votes, and should return false otherwise.
+- eliminate - The function takes an argument min, which will be the minimum number of votes that anyone in the election currently has.
+The function should eliminate the candidate (or candidates) who have min number of votes. */
+
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
