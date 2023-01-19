41e97b702ecee196706af24c3724ecf714234957
Irena Daneva <de.luxe9997@gmail.com>
2023-01-20 00:59:06 +0200

    Update log.sql

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -1,3 +1,11 @@
+-- The CS50 Duck has been stolen! The town of Fiftyville has called upon you to solve the mystery of the stolen duck. Authorities believe 
+-- that the thief stole the duck and then, shortly afterwards, took a flight out of town with the help of an accomplice. Your goal is to identify:
+
+-- Who the thief is,
+-- What city the thief escaped to, and
+-- Who the thief’s accomplice is who helped them escape
+
+
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 SELECT day, month, year, street, description
